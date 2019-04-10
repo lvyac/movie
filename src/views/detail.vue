0dbb0aa6377f8ab112632e2bba354dcac065142b
lvyac <546651420@qq.com>
2019-04-10 23:10:30 +0800

    详情页

--- src/views/detail.vue
+++ src/views/detail.vue
@@ -1,7 +1,60 @@
 <template>
-    <div>
-        <div>这里是详情页</div>
-        <div>cid{{ $route.params.cid }}</div>
+  <div>
+    <div class="header">
+      <img :src="data.homePicUrl" class="logo">
+      <div class="movie-info">
+        <div class>{{data.name}}</div>
+        <div class>{{data.publishTime}}</div>
+      </div>
     </div>
+    <div class="content">{{content}}</div>
+    <div class="address"></div>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      data: {}
+    };
+  },
+  methods: {
+    gitDetail() {
+      let param = {
+        cid: this.$route.params.cid,
+        id: this.$route.params.id
+      };
+      this.$api.movieDetail(param, res => {
+        console.log(res);
+        this.data = res;
+      });
+    }
+  },
+  computed: {
+    content: function() {
+      let str = this.data.content;
+      if(str){
+          str = str.replace(/\r\n/g,"<br>");
+      }
+      
+      return str
+    }
+  },
+  created() {
+    this.gitDetail();
+  }
+};
+</script>
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  flex-direction: row;
+}
+.logo {
+  width: 120px;
+  height: 170px;
+}
+</style>
+
+
 
